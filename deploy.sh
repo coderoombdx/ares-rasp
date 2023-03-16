7739668c3bdb26105e0e0446fe1b2a38cc745b58
FEUD7506 <frederic.denis@orange.com>
2023-03-16 12:43:22 +0100

    [ENIGME] Ajoute la data class PropositionEnigme

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 SERVER_URL="192.168.1.10"
 
+ping $SERVER_URL
+
 echo "scp du jar"
 ls build/libs/ares-0.0.1-SNAPSHOT.jar
-scp build/libs/ares-0.0.1-SNAPSHOT.jar ares@192.168.1.10:ares.jar
+scp build/libs/ares-0.0.1-SNAPSHOT.jar ares@${SERVER_URL}:ares.jar
 sleep 10
